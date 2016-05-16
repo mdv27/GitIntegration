f74b255e3ab8273419398ea196674e608029d078
Madhusudan Vishnupurikar <vishnupurikarmd@gmail.com>
2016-05-16 23:08:25 +0530

    get input and add

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to main");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        int i1 = scanner.nextInt();
+        System.out.println(i + i1);
     }
 }
